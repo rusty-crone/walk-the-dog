c1be2d676eebc0c3959e72ff9f835730095cf44f
Todd W Crone <tcrone@newrelic.com>
2023-07-19 12:55:10 -0400

    Add the infamous black triangle

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,32 @@ use web_sys::console;
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.move_to(300.0, 0.0);
+    context.begin_path();
+    context.line_to(0.0, 600.0);
+    context.line_to(600.0, 600.0);
+    context.line_to(300.0, 0.0);
+    context.close_path();
+    context.stroke();
+    context.fill();
 
     // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    console::log_1(&JsValue::from_str("Done son!"));
 
     Ok(())
 }
